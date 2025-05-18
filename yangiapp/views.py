277aa7ec4885516f1257a6c951e2b11bd841c53a
ValijonPoyonov <valijonpoyonov@gmail.com>
2025-05-18 20:25:12 +0500

    copying from teacher
    Signed-off-by: ValijonPoyonov <valijonpoyonov@gmail.com>

--- yangiapp/views.py
+++ yangiapp/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Countries, Employees, Dependents
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = "blog/post/list.html"
+
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug, status="published", publish__yead=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
 
 
 def index_page(request):
--- yangiapp/yangi_urls.py
+++ yangiapp/yangi_urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from . import views
 from .views import index_page, get_max_salary_employees, get_dependents
 
+
+app_name = 'blogapp'
 urlpatterns = [
+path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+         views.post_detail,
+         name='post_detail'),
+
     # path('', list1, name='countries_list'),
-    path('', index_page, name='index_list'),
-    path('salary/<int:top>',get_max_salary_employees, name='employee-list'),
-    path('deps/<int:employee_id>', get_dependents, name='deps-list'),
+    # path('', index_page, name='index_list'),
+    # path('salary/<int:top>',get_max_salary_employees, name='employee-list'),
+    # path('deps/<int:employee_id>', get_dependents, name='deps-list'),
 ]
